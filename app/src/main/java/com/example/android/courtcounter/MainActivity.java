e8085d56b4a636c5347d052141df1743767dd658
karthikr1901 <karthikr1901@gmail.com>
2017-02-01 22:14:45 +0800

    First Android Commit

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    int scoreB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
