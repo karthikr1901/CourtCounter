634823009725f0b79f97893d2afa02fd45bb077b
karthikr1901 <karthikr1901@gmail.com>
2017-02-04 23:07:21 +0800

    Display points for team B method

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -52,4 +52,12 @@ public class MainActivity extends AppCompatActivity {
         scoreA = scoreA + 1;
         displayForTeamA(scoreA);
     }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
 }
